1768db20601b0e2b7e5e619772887a63337814e6
Alexey <lisenkov0a@gmail.com>
2022-10-15 16:06:16 +0700

    Not understand this solve.

--- src/main/java/Exercize1.java
+++ src/main/java/Exercize1.java
@@ -1,5 +1,14 @@
-public class Exercize1 {
-    public static void main(String[] args) {
+import java.util.HashMap;
 
+public class Exercize1 {
+    public static Map<Character, Integer> main(String[] args) {
+        public Map<Character, Integer> countDuplicateCharacters(String str) {
+            Map<Character, Integer> result = new HashMap<>();
+            for (int i = 0; i<str.length(); i++){
+                char ch = str.charAt(i);
+                result.compute(ch, (k, v) -> (v == null) ? l : ++v);
+            }
+            return result;
+        }
     }
 }
